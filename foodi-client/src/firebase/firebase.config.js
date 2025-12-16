5d631f9fab7e5d68ad1dcdf85d879089c2316cfc
chayanhati <chayanhati2004@gmail.com>
2025-12-16 14:32:05 +0530

    Fix Firebase Hosting and deploy React app

--- foodi-client/src/firebase/firebase.config.js
+++ foodi-client/src/firebase/firebase.config.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import.meta.env.VITE_APIKEY
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APIID,
 };
 
 const app = initializeApp(firebaseConfig);
+
+export default app;          // ✅ ADD THIS
 export const auth = getAuth(app);
